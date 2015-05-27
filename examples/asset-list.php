fe3fe50288c4949a62252169868055774184d038
Ivan Wolf <iwolf@detailag.ch>
2015-05-27 11:23:02 +0200

    Added params for filtering and sorting

--- examples/asset-list.php
+++ examples/asset-list.php
@@ -5,13 +5,29 @@ use Lwi\Client\InsideClient;
 $config = require 'bootstrap.php';
 $params = array();
 
+// Example: ?page=2
 if (isset($_GET['page'])) {
     $params['page'] = (int) $_GET['page'];
 }
 
+// Example: &page_size=20
 if (isset($_GET['page_size'])) {
     $params['page_size'] = (int) $_GET['page_size'];
 }
+
+// Example: &query=auge
+if (isset($_GET['query'])) {
+    $params['query'] = (string) $_GET['query'];
+}
+
+$params['filter'] = array(
+    array(
+        'property' => 'type',
+        'value' => 'fd6f26da-4f34-4316-81b6-69bf0c5f2a92', // Key Visual
+        'operator' => '=', // equals
+        'type' => 'string',
+    ),
+);
 
 $client = InsideClient::factory($config);
 
--- src/Lwi/Client/ServiceDescription/Inside.php
+++ src/Lwi/Client/ServiceDescription/Inside.php
@@ -7,7 +7,6 @@ return array(
             'httpMethod'       => 'GET',
             'uri'              => 'assets',
             'summary'          => 'List assets',
-//            'documentationUrl' => 'http://tbd',
             'parameters'       => array(
                 'page' => array(
                     'description' => 'The number of the page',
@@ -16,10 +15,22 @@ return array(
                     'required'    => false,
                 ),
                 'page_size' => array(
-                    'description' => 'The number of assets to list on a page',
+                    'description' => 'The number of items to list on a page',
                     'location'    => 'query',
                     'type'        => 'integer',
                     'required'    => false,
+                ),
+                'query' => array(
+                    'description' => 'Full text search query (currently searches only in asset name)',
+                    'location'    => 'query',
+                    'type'        => 'string',
+                    'required'    => false,
+                ),
+                'filter' => array(
+                    '$ref' => 'FilterParam',
+                ),
+                'sort' => array(
+                    '$ref' => 'FilterParam',
                 ),
             ),
             'responseClass' => 'Lwi\Client\Response\AssetList',
@@ -28,7 +39,6 @@ return array(
             'httpMethod'       => 'GET',
             'uri'              => 'assets/{asset_id}',
             'summary'          => 'Fetch an asset',
-//            'documentationUrl' => 'http://tbd',
             'parameters'       => array(
                 'asset_id' => array(
                     'description' => 'The ID of the asset you wish to fetch',
@@ -38,6 +48,66 @@ return array(
                 ),
             ),
             'responseClass' => 'Lwi\Client\Response\Asset',
+        ),
+    ),
+    'models' => array(
+        'Filter' => array(
+            'type' => 'object',
+            'properties' => array(
+                'property' => array(
+                    'description' => 'The property to filter against',
+                    'type'        => 'string',
+                    'required'    => true,
+                ),
+                'value' => array(
+                    'description' => 'The value to filter against',
+                    'type'        => 'string',
+                    'required'    => true,
+                ),
+                'operator' => array(
+                    'description' => 'The operator the use for filtering',
+                    'type'        => 'string',
+                    'required'    => false,
+                ),
+                'type' => array(
+                    'description' => 'The data type of the value',
+                    'type'        => 'string',
+                    'required'    => false,
+                ),
+            ),
+        ),
+        'FilterParam' => array(
+            'description' => 'An array of filters',
+            'location'    => 'query',
+            'type'        => 'array',
+            'required'    => false,
+            'items'       => array(
+                '$ref' => 'Filter',
+            ),
+        ),
+        'Sort' => array(
+            'type' => 'object',
+            'properties' => array(
+                'property' => array(
+                    'description' => 'The property use for sorting',
+                    'type'        => 'string',
+                    'required'    => true,
+                ),
+                'direction' => array(
+                    'description' => 'The sorting direction (either "asc" or "desc")',
+                    'type'        => 'string',
+                    'required'    => false,
+                ),
+            ),
+        ),
+        'SortParam' => array(
+            'description' => 'An array of sorters',
+            'location'    => 'query',
+            'type'        => 'array',
+            'required'    => false,
+            'items'       => array(
+                '$ref' => 'Sort',
+            ),
         ),
     ),
 );
