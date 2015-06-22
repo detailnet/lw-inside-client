3747c9a7ef67b4efdd006150224b1cdfa79b60c6
Steeve Christen <schristen@detailag.ch>
2015-06-22 13:36:40 +0200

    Removed query parameter

--- src/Lwi/Client/ServiceDescription/Inside.php
+++ src/Lwi/Client/ServiceDescription/Inside.php
@@ -88,12 +88,6 @@ return array(
                 'page_size' => array(
                     '$ref' => 'PageSizeParam',
                 ),
-                'query' => array(
-                    'description' => 'Full text search query (currently searches only in product name)',
-                    'location'    => 'query',
-                    'type'        => 'string',
-                    'required'    => false,
-                ),
                 'filter' => array(
                     '$ref' => 'FilterParam',
                 ),
